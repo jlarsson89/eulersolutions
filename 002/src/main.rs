b9264c465fb1ed66999b23688b136a7aace86ec0
Johan Larsson <johan.larsson1989@gmail.com>
2017-02-19 18:05:27 +0000

    changed variables to i32 instead of u32, made some readability changes

--- 002/src/main.rs
+++ 002/src/main.rs
@@ -1,15 +1,17 @@
 fn main() {
-    let mut a: u32 = 0;
-    let mut b: u32 = 1;
-    let mut tot: u32 = 0;
+    let mut a: i32 = 0;
+    let mut b: i32 = 1;
+    let mut total: i32 = 0;
     loop {
         let temp = a + b;
         a = b;
         b = temp;
-        if a >= 10000000 { break; }
+        if a >= 10000000 { 
+        	break;
+        }
         if (a % 2 == 0) && (a < 4000000) {
-            tot += a;
+            total += a;
         }
     }
-    println!("{}", tot);
+    println!("{}", total);
 }
