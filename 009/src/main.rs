8440ea5d12f5c87e8804578e5e05692baf77ff2d
Johan Larsson <johan.larsson1989@gmail.com>
2017-02-21 22:28:37 +0000

    added problem 009, needs optimisation

---
+++ 009/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    for a in 1..1000 {
+        for b in 1..1000 {
+            for c in 1..1000 {
+                if a * a + b * b == c * c && a + b + c == 1000 {
+                    println!("{}", a * b * c);
+                    break;
+                }
+            }
+        }
+    }
+}
